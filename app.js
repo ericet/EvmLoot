ec3ab38a1859b467a122f009ef26e16e9c1cde70
ericet <ericetchen@gmail.com>
2021-10-20 22:53:14 -0400

    EvmLoot frontend

---
+++ app.js
@@ -0,0 +1,798 @@
+const env = "testnet";
+const chainId = 9000;//test net
+const price = 0;
+let address;
+
+const contractAddress = "0xA037171E0d8fa87FC935CC627c47825375F17F36";//main net
+const etherscanUrl = "https://evm.evmos.org/tx";//main net
+let provider = null;
+
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "symbol",
+        type: "string",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "approved",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "bool",
+        name: "approved",
+        type: "bool",
+      },
+    ],
+    name: "ApprovalForAll",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenAmount",
+        type: "uint256",
+      },
+    ],
+    name: "claim",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getApproved",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getCoin",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getCoinInfo",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getCool",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getValues",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+    ],
+    name: "isApprovedForAll",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "mintPrice",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenAmount",
+        type: "uint256",
+      },
+    ],
+    name: "ownerClaim",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "ownerOf",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "safeTransferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes",
+        name: "_data",
+        type: "bytes",
+      },
+    ],
+    name: "safeTransferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+      {
+        internalType: "bool",
+        name: "approved",
+        type: "bool",
+      },
+    ],
+    name: "setApprovalForAll",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "mintPrice_",
+        type: "uint256",
+      },
+    ],
+    name: "setMintPrice",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes4",
+        name: "interfaceId",
+        type: "bytes4",
+      },
+    ],
+    name: "supportsInterface",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "index",
+        type: "uint256",
+      },
+    ],
+    name: "tokenByIndex",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "index",
+        type: "uint256",
+      },
+    ],
+    name: "tokenOfOwnerByIndex",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "tokenURI",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+window.onload = () => {
+  var animateButton = function (e) {
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove("animate");
+
+    e.target.classList.add("animate");
+    setTimeout(function () {
+      e.target.classList.remove("animate");
+    }, 700);
+  };
+
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener("click", animateButton, false);
+  }
+
+  window?.ethereum?.on("disconnect", () => {
+    window.location.reload();
+  });
+
+  window?.ethereum?.on("networkChanged", () => {
+    window.location.reload();
+  });
+
+  window?.ethereum?.on("chainChanged", () => {
+    window.location.reload();
+  });
+
+  const connectWallet = async () => {
+    await window.ethereum.enable();
+    if (Number(window.ethereum.chainId) !== chainId) {
+      return failedConnectWallet();
+    }
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.send("eth_requestAccounts");
+    document.getElementById("button").innerHTML = accounts[0];
+  };
+
+
+
+  const failedConnectWallet = () => {
+    document.getElementById("button").innerHTML = "Error Network, switch to Evmos";
+  };
+
+  const switchNetwork = async () => {
+    if (env === "test") {
+      window?.ethereum
+        ?.request({
+          method: "wallet_addEthereumChain",
+          params: [
+            {
+              chainId: "9000",
+              chainName: "Evmos Testnet",
+              nativeCurrency: {
+                name: "PHOTON",
+                symbol: "PHOTON",
+                decimals: 18,
+              },
+              rpcUrls: ["http://arsiamons.rpc.evmos.org:8545"],
+              blockExplorerUrls: ["https://evm.evmos.org"],
+            },
+          ],
+        })
+        .then(() => {
+          connectWallet();
+        })
+        .catch(() => {
+          failedConnectWallet();
+        });
+    } else {
+      window?.ethereum
+        ?.request({
+          method: "wallet_addEthereumChain",
+          params: [
+            {
+              chainId: "9000",
+              chainName: "Evmos Testnet",
+              nativeCurrency: {
+                name: "PHOTON",
+                symbol: "PHOTON",
+                decimals: 18,
+              },
+              rpcUrls: ["http://arsiamons.rpc.evmos.org:8545"],
+              blockExplorerUrls: ["https://evm.evmos.org"],
+            },
+          ],
+        })
+        .then(() => {
+          connectWallet();
+        })
+        .catch(() => {
+          failedConnectWallet();
+        });
+    }
+  };
+
+  document.getElementById("button").addEventListener("click", switchNetwork);
+
+  connectWallet();
+
+
+
+  const handleMint = async () => {
+    $.toast().reset("all");
+    if (!provider) {
+      connectWallet();
+    } else {
+      try {
+        document.getElementById("mint").innerHTML = "Minting...";
+        const signer = await provider.getSigner();
+        const account = await signer.getAddress();
+        const inputValue = document.getElementById("tokenId").value;
+        if (!inputValue || Math.round(inputValue) !== Number(inputValue)) {
+          document.getElementById("mint").innerHTML = "Mint";
+          return $.toast({
+            heading: "Error",
+            text: "Enter an integer！",
+            position: "top-center",
+            showHideTransition: "fade",
+            icon: "error",
+          });
+        } else if (inputValue > 10) {
+          document.getElementById("mint").innerHTML = "Mint";
+          return $.toast({
+            heading: "Error",
+            text: "Max amount 10！",
+            position: "top-center",
+            showHideTransition: "fade",
+            icon: "error",
+          });
+        }
+        const ImageContract = new ethers.Contract(contractAddress, abi, signer);
+        const amountRaw = ethers.utils.parseUnits(`${price * inputValue}`, 18).toString();
+        const balanceRaw = await provider.getBalance(account);
+        const balance = ethers.utils.formatUnits(balanceRaw, 18);
+        if (Number(balance) < price * inputValue) {
+          document.getElementById("mint").innerHTML = "Mint";
+          return $.toast({
+            heading: "Error",
+            text: "Insufficient balance！",
+            showHideTransition: "fade",
+            position: "top-center",
+            icon: "error",
+          });
+        }
+        const estimateGas = await ImageContract.estimateGas.claim(inputValue, {
+          value: amountRaw,
+        });
+        const gasLimit = Math.floor(estimateGas.toNumber() * 2);
+
+        const response = await ImageContract.claim(inputValue, {
+          value: amountRaw,
+          gasLimit,
+        });
+        $.toast({
+          heading: "Minting",
+          text: "Start to minting！",
+          position: "top-center",
+          showHideTransition: "fade",
+          hideAfter: 10000,
+          icon: "info",
+        });
+        const result = await response.wait();
+        let images=[];
+        for(let event of result.events){
+          let tokenId = event.args.tokenId;
+          let tokenUri = await ImageContract.tokenURI(tokenId.toNumber());
+          let json = atob(tokenUri.substring(29));
+          let result = JSON.parse(json);
+          images.push(result.image);
+        }
+        let html =`<h2>You Minted:</h2>
+        <div class="row">`;
+        for(let image of images){
+          html +=`<div class="column">
+          <img src="${image}" width="400" height="300">
+          </div>`;
+        }
+        html +='</div>';
+        $('div#minted').html(html);
+        $.toast().reset("all");
+        $.toast({
+          heading: "Success",
+          text: "Minted Success!",
+          showHideTransition: "slide",
+          position: "top-center",
+          icon: "success",
+        });
+        document.getElementById("mint").innerHTML = "Mint";
+        // window.open(`${etherscanUrl}/${result.transactionHash}`);
+      } catch (e) {}
+    }
+  };
+  document.getElementById("mint").addEventListener("click", handleMint);
+};
